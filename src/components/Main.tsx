e7c077ec53e989c23d0d60342c482e8c7f91d90e
Raulik <raul.hajiyev95@gmail.com>
2024-01-06 15:43:50 -0500

    create basic layout

---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>Main here</div>
+  )
+}
+
+export default Main
---
+++ src/components/navBar/Logo.tsx
@@ -0,0 +1,10 @@
+import { Box, Center, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <Center> </Center>
+  )
+}
+
+export default Logo
