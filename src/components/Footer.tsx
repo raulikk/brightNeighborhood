e06b0ada521b8fb671c353c5d652b4456ff035be
Raulik <raul.hajiyev95@gmail.com>
2024-01-10 16:06:06 -0500

    set up a simple responsive footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
-import { Flex, VStack, Text, Stack, Image, Box } from '@chakra-ui/react'
+import { Flex, VStack, Text, Stack, Image, Box, Heading } from '@chakra-ui/react'
 import React from 'react'
 import Logo from './header/Logo'
 import footerLogo from '../assets/logo/bnLogoHorizontal.svg'
 const Footer = () => {
   return (
    <Flex
-    bg="green"  
-    position="relative" 
+    bg="#F4A460"  
+    position="relative"
+    
+    padding={"2.5rem 20px"}
     direction={{
     lg:"row", 
     md: "column",
@@ -14,15 +16,29 @@ const Footer = () => {
     base:"column"
    }}  
    
-   justifyContent={'space-between'} w="100vw" h="250px">
+   justifyContent={'space-between'} 
+   
+   alignItems={{
+    md:"start",
+    sm:"start", 
+    base:"start"
+
+   }}
+   
+   w="100vw" h="250px">
     
-
-    <Box position="absolute" left="20px" border="1px solid gray" padding="5px" > 
-    <Text>Move Up  </Text>  </Box> 
-    <Image src={footerLogo} h="70px" w="auto" position="absolute" border="2px solid coral" top="50px"  left="10px"/> 
+    <Box position="absolute" right="2.5rem" top="0" border="1px solid gray" padding="5px" > 
+    <Text>Move Up  </Text>  
+    </Box> 
+    <Image src={footerLogo} h="50px" w="auto"  border="2px solid coral" bg="white" /> 
+    
     <Stack > 
-
-
+    <Heading > 
+        Follow Us 
+    </Heading>
+    <Text>Facebook</Text>
+    <Text>Twitter</Text>
+    <Text>Instagram</Text>
 
     </Stack>
    </Flex>
