fa6b5c8472d17d6d7798b5647d1e3212692a1b9f
Raulik <raul.hajiyev95@gmail.com>
2024-02-04 16:55:54 -0500

    Create Basic layout of the Events page

---
+++ src/components/EventItem.tsx
@@ -0,0 +1,62 @@
+import {Card, CardHeader,CardBody,CardFooter, Stack, Heading, Text, Image, HStack, Icon, Divider, VStack, Button, Flex } from "@chakra-ui/react"
+import Temporary from '../assets/img/environment.webp'
+import { FaLocationDot } from "react-icons/fa6";
+import { SiEventbrite } from "react-icons/si";
+
+const EventItem = () => {
+  return (
+    <Card bg="coral"  
+    
+    overflow="hidden"
+
+    minWidth={"20rem"}
+   
+    borderRadius={"25px"}
+    > 
+    <CardBody padding={0}> 
+        <Image src={Temporary} overflow="hidden"   />
+    <Stack padding="10px"> 
+        <Heading   
+            size={{
+                xl:"lg",
+                lg:"lg",
+                sm:"xl", 
+                base:"lg"
+            }}
+       > Lead In Water & The Solution 
+       </Heading>
+        <Text 
+        fontSize={{
+            xl:"2rem" ,
+            base:"1.5rem" 
+        }}
+        >February 28th, 5pm- 6pm </Text>
+        <HStack> 
+            <Icon as={FaLocationDot} />
+            <Text> 8560 South Cottage Grove Avenue Chicago, IL 60619</Text>
+        </HStack>
+        <Divider  />
+        <CardFooter padding="10px" > 
+           <Flex w="100%"  justifyContent={"end"}> 
+                <HStack> 
+                   
+                    <Button bg="none" border={"1px solid white"} borderRadius={"25px"} > 
+                     Learn More 
+                     <Icon as={SiEventbrite}  marginLeft="5px" />
+                    </Button>
+                </HStack>
+            </Flex>
+
+
+        </CardFooter>
+    </Stack>
+
+
+        </CardBody>
+
+
+    </Card>
+  )
+}
+
+export default EventItem
--- src/components/pages/EventPage.tsx
+++ src/components/pages/EventPage.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { SimpleGrid, Text, VStack, Box } from '@chakra-ui/react'
+import EventItem from '../EventItem'
 
 const EventPage = () => {
   return (
-    <div>EventPage</div>
+     <VStack margin={
+      {
+        xl: "80px 40px", 
+      }
+
+     }
+     padding={{
+      base:"1rem"
+     }}
+     
+     > 
+    <SimpleGrid columns={{
+      xl: 3,
+      lg:3, 
+      md:2, 
+      sm: 1, 
+      base:1
+
+    }} gap="4"  border={"2px solid green"}> 
+    
+    <EventItem /> 
+    <EventItem />   
+    <EventItem /> 
+    
+    
+   
+
+
+    </SimpleGrid>
+    <Text> https://www.eventbrite.com/e/lets-discuss-lead-in-our-water-tickets-817558317937?aff=oddtdtcreator</Text>
+   
+   
+   
+    </VStack>
   )
 }
 
